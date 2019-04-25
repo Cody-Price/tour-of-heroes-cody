c3c5ea8b488bbeb18fcdc1df854bfb2a0aa52513
Cody Price <ctprice11@gmail.com>
2019-04-25 13:25:41 -0600

    Change default style to css from scss

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,7 +4,7 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = {
